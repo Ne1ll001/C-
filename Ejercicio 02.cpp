9a04e31495afe3ac96fe639d0d5452d63c8d2dc0
Ne1ll001 <104156215+Ne1ll001@users.noreply.github.com>
2022-04-27 22:18:26 -0500

    Create Ejercicio 02.cpp

---
+++ Ejercicio 02.cpp
@@ -0,0 +1,12 @@
+//2. Calcule los primeros 50 números primos y muestre el resultado en pantalla.
+
+int main()
+{
+    for(int i=1;i<=50;i++){
+        if(50 % i == 0 )
+            if(50 == i)
+                cout<<i<<endl;
+    }
+
+    return 0;
+}
