003c07fe296a811ea618a3826a262945ce4e9e9c
Ne1ll001 <104156215+Ne1ll001@users.noreply.github.com>
2022-04-27 22:13:13 -0500

    Create Ejercicio 08.cpp

---
+++ Ejercicio 08.cpp
@@ -0,0 +1,16 @@
+//8.Escribir un programa que calcule la media de x cantidad números introducidos por el teclado.
+
+int main()
+{
+    int a;
+    int num;
+    cout<<"Ingrese la cantidad de numeros que desee introducir: "<<endl;
+    cin>>a;
+
+    for(int i=0;i<a;i++){
+        cout<<"Ingrese un numero: "<<endl;
+        cin>>num++
+    }
+
+    return 0;
+}
