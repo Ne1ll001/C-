a66a133b53611daf1eb5ab575ddd33ad809f1966
Ne1ll001 <104156215+Ne1ll001@users.noreply.github.com>
2022-04-27 21:55:52 -0500

    Create Ejercicio 01.cpp

---
+++ Ejercicio 01.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int a;
+    int b;
+    int p;
+    cout<<"Ingrese dos numeros enteros: "<<endl;
+    cin>>a;
+    cin>>b;
+
+    p=a*b;
+    cout<<"El producto de ambos numeros enteros es: "<<p<<endl;
+
+    return 0;
+}
