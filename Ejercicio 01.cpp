6fc6fd7fedefa978c7da7a1963f04c89573bc987
Ne1ll001 <104156215+Ne1ll001@users.noreply.github.com>
2022-04-27 22:06:48 -0500

    Create Ejercicio 01.cpp

---
+++ Ejercicio 01.cpp
@@ -0,0 +1,17 @@
+//Laboratorio 01
+//1. Escriba un código que solicite al usuario ingresar dos números enteros y que muestre el producto de ambos.
+
+int main()
+{
+    int a;
+    int b;
+    int p;
+    cout<<"Ingrese dos numeros enteros: "<<endl;
+    cin>>a;
+    cin>>b;
+
+    p=a*b;
+    cout<<"El producto de ambos numeros enteros es: "<<p<<endl;
+
+    return 0;
+}
