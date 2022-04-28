20cff4c85175bda01fca72d4cf21845eb3378c18
Ne1ll001 <104156215+Ne1ll001@users.noreply.github.com>
2022-04-27 22:08:07 -0500

    Create Ejercicio 02.cpp

---
+++ Ejercicio 02.cpp
@@ -0,0 +1,19 @@
+//2.Escriba un código que solicite el primer nombre de una persona, el apellido paterno y el apellido materno. Retornar su correo UNSA generado, el cual consiste 
+//  de la primera letra del nombre, el apellido paterno completo, y la primera letra del apellido materno. (se agrega el dominio de la universidad al final).
+
+int main()
+{
+    string n;
+    string p;
+    string m;
+    cout<<"Ingrese su primer nombre: "<<endl;
+    cin>>n;
+    cout<<"Ingrese el apellido paterno: "<<endl;
+    cin>>p;
+    cout<<"Ingrese el apellido materno: "<<endl;
+    cin>>m;
+
+    cout<<"unsa.edu.pe"<<endl;
+
+    return 0;
+}
